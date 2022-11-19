d50b4a6f48d2d0157b029133583cb96f317b2978
Lawrence <lquarshie29@gmail.com>
2022-11-18 23:46:37 -0500

    Added a .py file with casting by declaring the data in a particular way

---
+++ 6.py
@@ -0,0 +1,10 @@
+# Dealing with casting in python - specifying the data type of a variable
+# specification in how the data is entered
+
+a = 29
+b = '29'
+c = 29.0
+
+print(type(a))
+print(type(b))
+print(type(c))
