61ea166aea4abb3361fd759f02c162a11461a232
Lawrence <lquarshie29@gmail.com>
2022-11-20 10:06:22 -0500

    Added a .py file with casting where the data type function int, str and float are used

---
+++ 8.py
@@ -0,0 +1,12 @@
+# This is python code on casting
+a = int(3) # this is a as an integar
+b = float(3) # this is b as a float
+c = str(3) # this is c as a string
+
+print(a) # prints the output as an integer a = 3
+print(b) # prints the output as a float a = 3.
+print(c) # prints the output as a string a = 3
+
+print(type(a)) # prints the type of output, type = int
+print(type(b)) # prints the type of output, type = float
+print(type(c)) # prints the type of output, type = str
