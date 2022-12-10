1a9337da54205dfd04b1ddb740d6e48233eabcc1
Lawrence <lquarshie29@gmail.com>
2022-12-10 16:10:03 -0500

    Added 2 .py files which does concatenation using the '+' sign

---
+++ h.py
@@ -0,0 +1,5 @@
+first_word = "Python "
+second_word = "is "
+third_word = "awesome"
+
+print(first_word + second_word + third_word)
---
+++ h.v1.py
@@ -0,0 +1,5 @@
+first_name = "Lawrence "
+middle_name = "Kojo "
+last_name = "Quarshie"
+
+print(first_name + middle_name + last_name)
