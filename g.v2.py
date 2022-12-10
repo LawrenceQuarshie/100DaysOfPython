85fbcc050b0842e094222c69d027370747d93079
Lawrence <lquarshie29@gmail.com>
2022-12-10 16:01:41 -0500

    Added 2 more .py files with demonstration as in g.py

---
+++ g.v2.py
@@ -0,0 +1,6 @@
+fir_name = "Application"
+sec_name = "Specific"
+thi_name = "Integrated"
+fou_name = "Circuit"
+
+print(fir_name, sec_name, thi_name, fou_name)
---
+++ g.v3.py
@@ -0,0 +1,5 @@
+fir_name = "Instruction"
+sec_name = "Set"
+thi_name = "Architecture"
+
+print(fir_name, sec_name, thi_name)
