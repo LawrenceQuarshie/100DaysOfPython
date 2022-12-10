d12e978269c2fd0a40426db23b99794d6b7f528f
Lawrence <lquarshie29@gmail.com>
2022-12-10 14:34:54 -0500

    Added another variant of e.py which is also variant of d.py

---
+++ e.v1.py
@@ -0,0 +1,7 @@
+my_family = ["Lawrence", "Sandra", "Ellen", "Roland"]
+first_born, second_born, third_born, fourth_born = my_family
+
+print(first_born)
+print(second_born)
+print(third_born)
+print(fourth_born)
