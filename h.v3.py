cfe6a6e9c15112b5781de557d728ad0c0d6a13ea
Lawrence <lquarshie29@gmail.com>
2022-12-10 16:21:42 -0500

    Added 2 .py files which are instances of h.py

---
+++ h.v3.py
@@ -0,0 +1,6 @@
+fir_word = "Application "
+sec_word = "Specific "
+thi_word = "Integrated "
+fou_word = "Circuit"
+
+print(fir_word + sec_word + thi_word + fou_word)
---
+++ h.v4.py
@@ -0,0 +1,5 @@
+firs_word = "Instruction "
+seco_word = "Set "
+thir_word = "Architecture"
+
+print(firs_word + seco_word + thir_word)
