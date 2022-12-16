bd37cfb35e18e277a24bb3611fa442813b10c304
Lawrence <lquarshie29@gmail.com>
2022-12-16 18:39:53 -0500

    Added another .py file with complex number manipulations

---
+++ q.v2.py
@@ -0,0 +1,19 @@
+aa = 2 + 3j
+bb = 3 + 4j
+
+cc = aa + bb
+dd = aa - bb
+ee = aa * bb
+ff = aa / bb
+
+print(cc)
+print(type(cc))
+print("")
+print(dd)
+print(type(dd))
+print("")
+print(ee)
+print(type(ee))
+print("")
+print(ff)
+print(type(ff))
