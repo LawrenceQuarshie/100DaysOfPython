eb6015889ebd7015b29ea12f06ad7ccc4d32ce15
Lawrence <lquarshie29@gmail.com>
2022-12-23 15:34:05 -0500

    Added a .py file which is another demonstration of tuples as in s.py

---
+++ s.v1.py
@@ -0,0 +1,4 @@
+prog_lang = ("javascript", "python", "php", "sql", "java", "go", "matlab")
+
+print(prog_lang) # prints the output of prog_lang
+print(type(prog_lang)) # prints the data type of prog_lang
