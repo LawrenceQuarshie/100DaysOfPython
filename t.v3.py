87112e785072333262615682c941653fba88043e
Lawrence <lquarshie29@gmail.com>
2022-12-27 22:28:44 -0500

    Added a .py file which also demonstrates the use of range

---
+++ t.v3.py
@@ -0,0 +1,7 @@
+div_by_five = range(-21, 21, 5) # sets a range for div_by_five
+
+div_by_five_ = list(div_by_five) # lists all elements of div_by_five
+
+print(div_by_five_) # prints all items in div_by_five_
+
+print(type(div_by_five_)) # prints the data type of div_by_five_
