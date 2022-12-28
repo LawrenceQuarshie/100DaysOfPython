6a607152387da011ed78ceb57e8a60bbf1188e19
Lawrence <lquarshie29@gmail.com>
2022-12-27 20:36:44 -0500

    Added a .py file which demonstrates the use of range() to generate a range of numbers

---
+++ t.v2.py
@@ -0,0 +1,7 @@
+even_number = range(0, 21, 2) # defines the range of numbers
+
+set_of_even_number = list(even_number) # lists all elements for the range of numbers
+
+print(set_of_even_number) # prints the list
+
+print(type(set_of_even_number)) # prints the data type of variable
