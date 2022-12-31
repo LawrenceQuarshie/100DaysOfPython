ca5374b504c53c7dcc9de2c036718389527ee844
Lawrence <lquarshie29@gmail.com>
2022-12-31 12:14:39 -0500

    Added another .py file which is another instance of use of dictionary in Python

---
+++ u.v2.py
@@ -0,0 +1,4 @@
+phone = {'make': 'Apple', 'model': 'iPhone', 'series': '14 Pro Max'}
+
+print(phone) # prints the dict to the screen
+print(type(phone)) # prints the data type for phone on to the screen
