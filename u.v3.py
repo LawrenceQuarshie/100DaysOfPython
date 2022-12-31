f366b6eec1f7677d4baf30e063a8223b20cd1095
Lawrence <lquarshie29@gmail.com>
2022-12-31 17:08:34 -0500

    Added a .py file which demonstrates the use of dictionaries in Python

---
+++ u.v3.py
@@ -0,0 +1,19 @@
+text_type = {'data0': 'str'}
+
+num_type = {'data0': 'int', 'data1': 'float', 'data2': 'complex'}
+
+seq_type = {'data0': 'list', 'data1': 'tuple', 'data2': 'range'}
+
+map_type = {'data0': 'dict'}
+
+print(text_type) # prints out the content of text_type
+print(type(text_type)) # prints out the content of text_type
+
+print(num_type) # prints out the content of num_type
+print(type(num_type)) # prints out the content of text_type
+
+print(seq_type) # prints out the content of seq_type
+print(type(seq_type)) # prints out the content of seq_type
+
+print(map_type) # prints out the content of text_type
+print(type(map_type)) # prints out the content of seq_type
