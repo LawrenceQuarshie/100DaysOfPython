d61146abf7dfc61d401f73c057d3809a3feb27ac
Lawrence <lquarshie29@gmail.com>
2023-02-02 19:33:52 -0500

    Added .py file which demonstrate 'dict' data type specification during variable declaration.

---
+++ 16v2.py
@@ -0,0 +1,5 @@
+company = dict(option1 = "Arm", option2 = "Cadence", option3 = "Synopsys")
+
+print(company) # prints out the data from company
+
+print(type(company)) # prints the data type of company
