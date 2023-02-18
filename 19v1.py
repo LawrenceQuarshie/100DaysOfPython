61199c431c72b0d3818f4b11b0adcd7b9ec399da
Lawrence <lquarshie29@gmail.com>
2023-02-18 03:53:28 -0500

    Added a .py file like 19.py.

---
+++ 19v1.py
@@ -0,0 +1,7 @@
+bulb = 6 # average number of lights in Apt 2
+bat_cap = 0.5 # battery capacity of my phone in Ah
+avg_curr = 3 + 2.5j # average current flow
+
+print(type(bulb))
+print(type(bat_cap))
+print(type(avg_curr))
