405d3bcb5d9d710bb3764b28ae48d6da672a9c18
Lawrence <lquarshie29@gmail.com>
2023-02-18 02:42:55 -0500

    Added a .py file which demonstrates the use of the Python keyword 'frozenset' for frozenset declaration.

---
+++ 18.py
@@ -0,0 +1,5 @@
+a = frozenset({"zigbee", "bluetooth", "matter"}) # frozenset of communication standards
+
+print(a) # prints out the values of a
+
+print(type(a)) # prints out the data type of a (frozenset)
