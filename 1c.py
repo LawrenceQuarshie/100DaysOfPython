03c713d4a96892e3c6c7926dff654db6516898be
Lawrence <lquarshie29@gmail.com>
2023-02-24 09:29:56 -0500

    Added a .py file which demonstrate other ways of using the data type 'float'.

---
+++ 1c.py
@@ -0,0 +1,14 @@
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(x) # prints the value of x to screen
+print(type(x)) # prints the data type of x to screen
+print("")
+
+print(y) # prints the value of y to screen
+print(type(y)) # prints the data type of y to screen
+print("")
+
+print(z) # prints the value of z to screen
+print(type(z)) # prints the data type of z to screen
