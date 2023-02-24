b3fd04dba6c84e54d4175b61496fe88e28bf2a44
Lawrence <lquarshie29@gmail.com>
2023-02-23 19:18:07 -0500

    Added a .py file which prints out values of data type 'float' and their data types in Python.

---
+++ 1b.py
@@ -0,0 +1,12 @@
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(x) # prints out the value of x to screen
+print(type(x)) # prints out the data type of x to screen
+
+print(y) # prints out the value of y to screen
+print(type(y)) # prints out the data type of y to screen
+
+print(z) # prints out the value of z to screen
+print(type(z)) # prints out the data type of z to screen
