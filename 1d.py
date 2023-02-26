2a438d3b1e77c3f92080ec7e0545efaefa484fe3
Lawrence <lquarshie29@gmail.com>
2023-02-26 07:27:15 -0500

    Added a .py file which demonstrates complex numbers in Python.

---
+++ 1d.py
@@ -0,0 +1,12 @@
+x = 3 + 5j
+y = 5j
+z = -5j
+
+print(x) # prints out the value of x
+print(type(x)) # prints out the datatype of x
+
+print(y) # prints out the value of x
+print(type(y)) # prints out the datatype of x
+
+print(z) # prints out the value of x
+print(type(z)) # prints out the datatype of x
