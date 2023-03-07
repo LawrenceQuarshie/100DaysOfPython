64dc6a5c98974e8977e6ec18454d7b09bbabcb62
Lawrence <lquarshie29@gmail.com>
2023-03-06 19:09:18 -0500

    Added a .py file which demonstrates the use 'int, float and complex' for number type specification in Python.

---
+++ 1e.py
@@ -0,0 +1,20 @@
+# convert from int to float:
+x = float(1)
+
+# convert from float to int:
+y = int(2.8)
+
+# convert from int to complex:
+z = complex(1)
+
+a = 3 + 5.8j
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+print(type(a))
