d6c1f6285ef89413aa067d388912a06cfe350515
Lawrence <lquarshie29@gmail.com>
2023-04-10 09:15:55 -0400

    Added a .py file which demonstrates the use of ''' for multi-line string.

---
+++ 1l.py
@@ -0,0 +1,6 @@
+c = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.''' # defines a variable and assigns a value to it
+
+print(c) # prints the value of c
