1ee3401d5fdd3fdd779bd4d00e743a4648e30b04
Lawrence <lquarshie29@gmail.com>
2023-04-10 08:57:52 -0400

    Added a .py file which demonstrates multilevel string in Python.

---
+++ 1k.py
@@ -0,0 +1,6 @@
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.""" # multi-line string in Python
+
+print(a) # prints the value of a to screen
