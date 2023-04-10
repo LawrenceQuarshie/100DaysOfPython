c4855d750ea79732fca9c54a4d8c72847b9c3de5
Lawrence <lquarshie29@gmail.com>
2023-04-10 09:08:46 -0400

    Added a .py file which also demonstrates multi-level string in Python.

---
+++ 1kv1.py
@@ -0,0 +1,9 @@
+b = """RECOM Power's RPX-0.5Q and RPX-1.5Q
+series buck converters both feature compact
+and low profile 3 by 5 by 1.6 millimeter
+packing with an integrated shielded inductor
+and wettable flanks to support automated
+optical inspection...See More
+"""
+
+print(b)
