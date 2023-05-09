c6c72d7c1ca56c6e537cf0ed0596411dda15edf8
Lawrence <lquarshie29@gmail.com>
2023-05-09 07:34:52 -0400

    Added a .py file which also demonstrates the inserting numbers in string with indexing.

---
+++ 08.py
@@ -0,0 +1,5 @@
+diffraction_arc_1 = 0.413105
+diffraction_arc_2 = 0.826211
+
+txt = "The first and second diffraction angles are {0} and {1} respectively."
+print(txt.format(diffraction_arc_1, diffraction_arc_2))
