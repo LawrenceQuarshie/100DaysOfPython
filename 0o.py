eb6a70dd5776c17d9f10e6d79897d7e8fc3062f3
Lawrence <lquarshie29@gmail.com>
2023-06-21 20:59:57 -0400

    Added a .py file which demonstrates the use of count in Python.

---
+++ 0o.py
@@ -0,0 +1,8 @@
+outline = "introduction, course objectives, general discussion on emissions, emissions facilities at CTC, emissions theory"
+
+print("Enter a word to search for:")
+
+x = input()
+result = outline.count(x)
+
+print(result, " result(s)")
