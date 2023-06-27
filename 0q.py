aa3ddda287769041a6abae424ba6766a1a97ca6c
Lawrence <lquarshie29@gmail.com>
2023-06-27 01:49:05 -0400

    Added .py files for counting a series the number of times a phrase appears in a string and the summation of a series in Python.

---
+++ 0q.py
@@ -0,0 +1,7 @@
+item = "Samsung Galaxy A24, Samsung Galaxy A34 5G, Samsung Galaxy A54 5G, Samsung Galaxy S23 Plus"
+
+print("What are you looking for today?...")
+search = input()
+
+result = item.count(search)
+print(result)
---
+++ 0r.py
@@ -0,0 +1,7 @@
+sum_of_numbers = 0
+lastnumber = 1000
+
+for x in range(lastnumber + 1):
+    sum_of_numbers = sum_of_numbers + x
+
+print(sum_of_numbers)
