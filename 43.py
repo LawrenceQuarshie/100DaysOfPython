f0c7ebbb5129d71463a506bdd881e4517f1bdd65
Lawrence <lquarshie29@gmail.com>
2024-01-07 00:58:10 -0500

    Added a .py file which demonstrates the use bool() in Python. Returns 'True' for non-empty variable and 'False' for empty variables.

---
+++ 43.py
@@ -0,0 +1,7 @@
+a = "Hello" # returns 'True' because it has content
+b = 15 # returns 'True' because it has content
+c = "" # returns 'False' because it has not content
+
+print(bool(a))
+print(bool(b))
+print(bool(c))
