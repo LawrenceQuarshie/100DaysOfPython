4d7bec913c48b2e85bb5b9dd43dc793f0d909def
Lawrence Quarshie <lquarshie29@gmail.com>
2024-04-23 08:48:07 -0400

    Added a .py file that demonstrates looping using for loop in Python.

---
+++ 78.py
@@ -0,0 +1,4 @@
+testing = [1, 2, 3, 4, 5] # defines an array
+
+for i in range(len(testing)):
+    print("chapter " + str(testing[i])) # adds string 'chapter' and concatenates it with string of all entries in testing based on i indexing.
