a3942ee93ba2f7549e130e05b6b84120c953c33a
Lawrence <lquarshie29@gmail.com>
2024-05-24 14:48:20 -0400

    Added a .py file that demonstrates the use of looping for joining as an alternative of joining lists in Python.

---
+++ 8d.py
@@ -0,0 +1,7 @@
+electronics_color_code = ["resistors", "capacitors", "inductors", "diodes"]
+word_length = [9, 10, 9, 6]
+
+for x in word_length:
+    electronics_color_code.append(x)
+
+print(electronics_color_code)
