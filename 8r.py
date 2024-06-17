199ca72dac817c13e9d752eead48988b367a50cf
Lawrence <lquarshie29@gmail.com>
2024-06-17 17:35:39 -0400

    Added a .py file that demmonstrates the use of '.extend()' method for extending a list by concatenating that list and another list.

---
+++ 8r.py
@@ -0,0 +1,5 @@
+system1 = ['force', 'velocity', 'mass', 'damping', 'compliance']
+system2 = ['voltage', 'current', 'inductance', 'resistance', 'capacitance']
+
+system1.extend(system2)
+print(system1)
