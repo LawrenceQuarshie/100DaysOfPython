3d7ed312397926345676a66506977c210abbaf3f
Lawrence Quarshie <lquarshie29@gmail.com>
2025-02-16 16:07:56 -0600

    Added a .py file that demonstrates the use of '.union()' method for joining sets in Python.

---
+++ 01j.py
@@ -0,0 +1,8 @@
+# Lessons in Industrial Instrumentation, version 2.33
+# Page 3284(3311 of 3311)
+page_numbers = {1078, 1198, 816, 816}
+txt = {"x-windows", "xd_scale", "xic", "xio"}
+
+set_combined = page_numbers.union(txt)
+
+print(set_combined)
