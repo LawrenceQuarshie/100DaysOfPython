818ed4a5738fa38a8147ade4d84acc2f0b5f2b99
Lawrence Quarshie <lquarshie29@gmail.com>
2025-02-18 21:26:22 -0600

    Added a .py file that demonstrates the joining of two (2) or more sets using '.union()' method in Python.

---
+++ 01o.py
@@ -0,0 +1,7 @@
+set1 = {"m", "n", "o", "p"}
+set2 = {1045, 1236, 2745}
+set3 = {"wireless", "transmitter"}
+set4 = {"samsung", "apple", "huawei"}
+
+myset1 = set1.union(set2, set3, set4)
+print(myset1)
