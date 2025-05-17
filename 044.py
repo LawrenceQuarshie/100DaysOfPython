4f35ab6784493ed42753e22454daa2ff9937df1c
Lawrence Quarshie <lquarshie29@gmail.com>
2025-05-16 19:08:38 -0500

    Added a .py file that demonstrates the use checking whether a dict contains a key or not in Python.

---
+++ 044.py
@@ -0,0 +1,9 @@
+phone = {
+    "brand": "Apple",
+    "model": "iPhone 7 Plus",
+    "year": 2016,
+    "screen size": 5.5
+}
+
+if "screen size" in phone:
+    print("Yes, 'screen size' is a key in the phone dictionary")
