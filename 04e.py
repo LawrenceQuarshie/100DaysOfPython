0e8d868fde3f15459a5be75faccece3ecd518c65
Lawrence <lquarshie29@gmail.com>
2025-05-22 18:32:57 -0500

    Added a .py file that demonstrates how to remove a key/value from a dict in Python using the '.pop()' method and an argument which is the key of the key/value pair.

---
+++ 04e.py
@@ -0,0 +1,11 @@
+component = {
+    "brand": "Texas Instruments",
+    "model": "JK Flip-flop with Preset and Clear",
+    "year": 1983
+}
+
+print(component)
+
+component.pop("model")
+
+print(component)
