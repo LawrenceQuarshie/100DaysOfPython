cc32fb4850d6ed49e2e7d3cc855a1191d3d25662
Lawrence <lquarshie29@gmail.com>
2025-06-05 17:28:21 -0500

    Added a .py file that demonstrates nested dictionaries in Python.

---
+++ 051.py
@@ -0,0 +1,22 @@
+price_drop = {
+    "prod1": {
+        "brand": "Apple",
+        "model": "iPhone 12 Mini",
+        "oldprice": 5768.75,
+        "newprice": 5365.04
+    },
+    "prod2": {
+        "brand": "Apple",
+        "model": "iPhone 13",
+        "oldprice": 9392.50,
+        "newprice": 7800.80
+    },
+    "prod3": {
+        "brand": "Apple",
+        "model": "iPhone 12 Mini",
+        "oldprice": 15700.00,
+        "newprice": 14800.00
+    }
+}
+
+print(price_drop)
