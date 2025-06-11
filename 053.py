88f05050b5f555c3b777032c4874fd958628d4c4
Lawrence <lquarshie29@gmail.com>
2025-06-10 23:28:37 -0500

    Added a .py file that demonstrates how a dictionary of dictionaries can be used to create a nested dictionary in Python.

---
+++ 053.py
@@ -0,0 +1,25 @@
+phone1 = {
+    "brand": "Apple",
+    "description": "iPhone X",
+    "oldprice": 3717.70,
+    "newprice": 3693.44
+}
+phone2 = {
+    "brand": "Apple",
+    "description": "iPhone XS Max",
+    "oldprice": 6337.50,
+    "newprice": 6208.80
+}
+phone3 = {
+    "brand": "Apple",
+    "description": "iPhone XR",
+    "oldprice": 4240.68,
+    "newprice": 4191.96
+}
+phones = {
+    "phone1": phone1,
+    "phone2": phone2,
+    "phone3": phone3
+}
+
+print(phones)
