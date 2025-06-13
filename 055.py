29beaa77ad81d122b09f9da40958af7f7147fcd6
Lawrence <lquarshie29@gmail.com>
2025-06-13 00:02:34 -0500

    Added a .py file that demonstrates how to access data or a value from a nested dictionary in Python.

---
+++ 055.py
@@ -0,0 +1,25 @@
+pricedrop = {
+    "prod1": {
+        "brand": "Samsung",
+        "description": "Galaxy A55",
+        "url": "https://bestpricegh.com/products/samsung-galaxy-a55-8gb-ram-128gb",
+        "oldprice": 6099.00,
+        "newprice": 6000.00
+    },
+    "prod2": {
+        "brand": "Samsung",
+        "description": "Galaxy A55",
+        "url": "https://giloshop.com/samsung-galaxy-a556-a55-8gb-ram-128gb-storage-5g/",
+        "oldprice": 6353.00,
+        "newprice": 6160.00
+    },
+    "prod2": {
+        "brand": "Samsung",
+        "description": "Galaxy A55",
+        "url": "https://bestpricegh.com/products/samsung-galaxy-a55-8gb-ram-256gb",
+        "oldprice": 6599.00,
+        "newprice": 6500.00
+    }
+}
+
+print(pricedrop["prod1"]["url"])
