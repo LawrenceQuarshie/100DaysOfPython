7c95890d9d629aba1ba7ddda24d435cf46b1e7ef
Lawrence <lquarshie29@gmail.com>
2025-06-15 08:14:44 -0500

    Added a .py file that demonstrates looping within a nested dictionary in Python.

---
+++ 058.py
@@ -0,0 +1,26 @@
+prod = {
+    "prod1": {
+        "name": "Tecno",
+        "description": "Camon 40",
+        "leastprice": 3285.10
+    },
+    "prod2": {
+        "name": "Samsung",
+        "description": "Galaxy A26",
+        "leastprice": 2299.00
+    },
+    "prod3": {
+        "name": "Samsung",
+        "description": "Galaxy A36",
+        "leastprice": 4330.00
+    }
+}
+
+for x, obj in prod.items():
+    print(x)
+    
+    for y in obj:
+        if isinstance(obj[y], float):
+            print(y + ":" + str(obj[y]))
+        else:
+            print(y + ":" + obj[y])
