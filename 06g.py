aa3db54abb48e62738f2e0ee2da73eeb5f0e1ffc
Lawrence <lquarshie29@gmail.com>
2025-07-28 05:57:26 -0500

    Added a .py file that demonstrates the use of 'and' in conditionals in Python.

---
+++ 06g.py
@@ -0,0 +1,6 @@
+lowprice = 1496.00 # 32GB storage option
+highprice = 2327.50 # 64GB storage option
+price1 = 2137.50
+
+if lowprice < price1 and  highprice > price1:
+    print("Price of product 1 is neither the lowest or highest.")
