78d2d6d56085776122b7dea1bd0a194d1fbd79b5
Lawrence <lquarshie29@gmail.com>
2025-08-07 11:51:41 -0500

    Added a .py file that demonstrates the use of nested if statements in Python.

---
+++ 06m.py
@@ -0,0 +1,8 @@
+price = 6389.00
+
+if price > 2000.00:
+    print("The price is greater than the lowest price")
+    if price > 4000:
+        print("and then the price is also greater than the average")
+    else:
+        print("The price is good is a good offer")
