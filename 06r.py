c2e1f9ddcda587b610bfe5d691230f10b5f3e946
Lawrence <lquarshie29@gmail.com>
2025-08-15 00:38:43 -0500

    Added a .py file that demonstrates the use of 'match' and 'case' in place of complex 'if', 'elif' and 'else' statements in Python.

---
+++ 06r.py
@@ -0,0 +1,10 @@
+color = 1
+match color:
+    case 1:
+        print("Ink Black")
+    case 2:
+        print("Titanium Gray")
+    case 3:
+        print("Veil White")
+    case 4:
+        print("Mirage Blue")
