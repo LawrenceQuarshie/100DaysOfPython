1b1df2647b7010efaf181a82c69d5589e4e6a97e
Lawrence <lquarshie29@gmail.com>
2025-08-14 23:08:52 -0500

    Added a .py file that demonstrates the use of 'match' and 'case' in place of complex 'if', 'elif' and 'else' statements in Python.

---
+++ 06q.py
@@ -0,0 +1,8 @@
+battery_capacity = 5200
+match battery_capacity:
+    case 5000:
+        print("Samsung Galaxy A05s")
+    case 6000:
+        print("Huawei Nova Y72S")
+    case 5200:
+        print("Tecno Spark 40")
