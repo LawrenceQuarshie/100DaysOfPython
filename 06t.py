898be25847d20bf0a0768789c39868ef2e19d9dd
Lawrence <lquarshie29@gmail.com>
2025-08-16 18:36:39 -0500

    Added a .py file that demonstrates the use of 'match' and 'case' for conditions which are in the form of strings in Python.

---
+++ 06t.py
@@ -0,0 +1,9 @@
+color = "black"
+
+match color:
+    case "white":
+        print("Color is white")
+    case "black":
+        print("Color is black")
+    case _:
+        print("Color does not exist for Apple iPhone 16e")
