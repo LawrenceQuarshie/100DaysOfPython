4f6a4083fa3d2942da407b966f03de38e1651157
Lawrence Quarshie <lquarshie29@gmail.com>
2025-10-05 12:31:58 -0500

    Added a .py file that demonstrates the use nested 'for' loops in Python.

---
+++ 07q.py
@@ -0,0 +1,6 @@
+gadgets = ["laptop", "television", "soundbar", "phone"]
+measured_quantities = ["voltage", "current", "resistance"]
+
+for i in gadgets:
+    for j in measured_quantities:
+        print(i, j)
