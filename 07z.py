7b8b19844619093b0f099fc21303c13deea28cd6
Lawrence Quarshie <lquarshie29@gmail.com>
2025-11-02 17:35:00 -0600

    Added a .py file that demonstrates the use of functions with more then one argument in Python.

---
+++ 07z.py
@@ -0,0 +1,8 @@
+def product_full_name(brand, model):
+    fullname = brand + " " + model
+    print(fullname)
+
+product_full_name("Apple", "iPhone")
+product_full_name("Samsung", "Galaxy")
+product_full_name("Google", "Pixel")
+product_full_name("ABB", "SP4 General Purpose - Enclosed Motor")
