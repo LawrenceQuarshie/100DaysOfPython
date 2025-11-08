a104a2a7ed7aa48523b6130b17271364ec8448d9
Lawrence Quarshie <lquarshie29@gmail.com>
2025-11-08 15:05:39 -0600

    Added a .py file that demonstrates the use of unordered list of arguments/parameters in Python.

---
+++ 087.py
@@ -0,0 +1,4 @@
+def product(brand, model, total):
+    print("There are " + str(total) + " " + brand + " " + model + " listings")
+
+product(brand = "Apple", model = "iPhone 17 Pro Max", total = 10)
