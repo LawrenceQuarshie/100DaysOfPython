4dfec666daf29880fe0e4aa7bbab7170011c5f21
Lawrence Quarshie <lquarshie29@gmail.com>
2025-11-08 10:47:18 -0600

    Added a .py file that demonstrates the use of ordered parameters with arguments in Python.

---
+++ 085.py
@@ -0,0 +1,5 @@
+def productpricing(lowest, highest):
+    print("Least price: " + str(lowest))
+    print("Price range: " + str(lowest) + " - " + str(highest))
+
+productpricing(lowest=6960.00, highest=16200.00)
